e1bc6ad02e96787c8a9dedf1b14fff189cdaa5fa
Rafael T <rt.lucena@outlook.com>
2024-05-05 16:43:20 -0300

    Slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//Função que muda a cor do site
 var cores = ['slategray', 'white', 'olivedrab'];
 var corAtual = 0;
 
@@ -5,3 +6,20 @@ function mudarCor() {
     document.body.style.backgroundColor = cores[corAtual % cores.length];
     corAtual++;
 }
+
+//SLIDESHOW
+let imagens=['./src/assets/Bicicleta1.jpg','./src/assets/Bicicleta2.jpg','./src/assets/Bicicleta3.jpg','./src/assets/Bicicleta4.jpg'];
+let index =0;
+let time= 3000;
+
+//FUNÇÃO SLIDESHOW
+function slideShow(){
+    document.getElementById('imgBanner').src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
